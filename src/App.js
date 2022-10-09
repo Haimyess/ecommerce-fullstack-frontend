578cb570c2918e31029079acdcd267ce3a65fa54
Haim Yeshurun <haimyess@gmail.com>
2022-10-09 16:55:32 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch("/api/products/all");
+        const data = await res.json();
+
+        setProducts(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        {products.map((product) => {
+          return (
+            <div key={product.product_id}>
+              <p>{product.product_name}</p>
+              <img src={product.product_img} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
