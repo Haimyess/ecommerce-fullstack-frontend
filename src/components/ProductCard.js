516fe83e79cf944270fd6ff16d64c676cdf244ef
Haim Yeshurun <haimyess@gmail.com>
2022-11-02 16:21:38 +0200

    fixed product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,6 +3,8 @@
 import React, { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
 
+import { Link } from "react-router-dom";
+
 import "../styles/productCard.css";
 
 // To display the popular products in the near future, we are planning on counting the number of click =s that are been made in each of the prodcut card depeneding on the product_id tageting the component.
@@ -18,26 +20,26 @@ const ProductCard = ({
   return (
     <div className='product-card-wrapper'>
       <div className='card'>
-        {/* image */}
-        <div className='product-image-wrapper'>
-          <img className='product-img' src={product.product_image} />
-        </div>
-
-        {/* info */}
-        <div className='product-card-info'>
-          <h3 className='product-title'>{product.product_name}</h3>
-          {/* <p className='product-info'>{product.product_description}</p> */}
-          <p className='product-price'>${product.product_price}</p>
-        </div>
-
-        {/* button */}
+        <Link className='hover' to={`/product/${product.product_id}`}>
+          {/* image */}
+          <div className='product-image-wrapper'>
+            <img className='product-img' src={product.product_image} />
+          </div>
+          {/* info */}
+          <div className='product-card-info'>
+            <h3 className='product-title'>{product.product_name}</h3>
+            {/* <p className='product-info'>{product.product_description}</p> */}
+            <p className='product-price'>${product.product_price}</p>
+          </div>
+          {/* button */}
+        </Link>
         <button
           className='product-btn'
           onClick={
             () => handleAdd(product)
             // onAdd(product)
           }>
-          Add to cart
+          Add to Cart
         </button>
       </div>
     </div>
